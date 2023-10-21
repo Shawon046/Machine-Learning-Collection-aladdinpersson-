c2bcab5015d354db3ab60e125b2f9e43d8279f73
Md Shahedul Haque <shawon56512@gmail.com>
2023-10-20 21:56:11 -0400

    Successfully implemented fc_gan

--- ML/Pytorch/GANs/1. SimpleGAN/fc_gan.py
+++ ML/Pytorch/GANs/1. SimpleGAN/fc_gan.py
@@ -70,7 +70,6 @@ opt_gen = optim.Adam(gen.parameters(), lr=lr)
 criterion = nn.BCELoss()
 writer_fake = SummaryWriter(f"logs/fake")
 writer_real = SummaryWriter(f"logs/real")
-step = 0
 
 def main():
     print('---------- Checking Version of Virtual Environment before starting ---------------')
@@ -78,51 +77,54 @@ def main():
     # print("Torch Vision version:",torchvision.__version__)
     print("Is CUDA enabled?",torch.cuda.is_available())
 
-    # for epoch in range(num_epochs):
-    #     for batch_idx, (real, _) in enumerate(loader):
-    #         real = real.view(-1, 784).to(device)
-    #         batch_size = real.shape[0]
+    step = 0
 
-    #         ### Train Discriminator: max log(D(x)) + log(1 - D(G(z)))
-    #         noise = torch.randn(batch_size, z_dim).to(device)
-    #         fake = gen(noise)
-    #         disc_real = disc(real).view(-1)
-    #         lossD_real = criterion(disc_real, torch.ones_like(disc_real))
-    #         disc_fake = disc(fake).view(-1)
-    #         lossD_fake = criterion(disc_fake, torch.zeros_like(disc_fake))
-    #         lossD = (lossD_real + lossD_fake) / 2
-    #         disc.zero_grad()
-    #         lossD.backward(retain_graph=True)
-    #         opt_disc.step()
 
-    #         ### Train Generator: min log(1 - D(G(z))) <-> max log(D(G(z))
-    #         # where the second option of maximizing doesn't suffer from
-    #         # saturating gradients
-    #         output = disc(fake).view(-1)
-    #         lossG = criterion(output, torch.ones_like(output))
-    #         gen.zero_grad()
-    #         lossG.backward()
-    #         opt_gen.step()
+    for epoch in range(num_epochs):
+        for batch_idx, (real, _) in enumerate(loader):
+            real = real.view(-1, 784).to(device)
+            batch_size = real.shape[0]
 
-    #         if batch_idx == 0:
-    #             print(
-    #                 f"Epoch [{epoch}/{num_epochs}] Batch {batch_idx}/{len(loader)} \
-    #                     Loss D: {lossD:.4f}, loss G: {lossG:.4f}"
-    #             )
+            ### Train Discriminator: max log(D(x)) + log(1 - D(G(z)))
+            noise = torch.randn(batch_size, z_dim).to(device)
+            fake = gen(noise)
+            disc_real = disc(real).view(-1)
+            lossD_real = criterion(disc_real, torch.ones_like(disc_real))
+            disc_fake = disc(fake).view(-1)
+            lossD_fake = criterion(disc_fake, torch.zeros_like(disc_fake))
+            lossD = (lossD_real + lossD_fake) / 2
+            disc.zero_grad()
+            lossD.backward(retain_graph=True)
+            opt_disc.step()
 
-    #             with torch.no_grad():
-    #                 fake = gen(fixed_noise).reshape(-1, 1, 28, 28)
-    #                 data = real.reshape(-1, 1, 28, 28)
-    #                 img_grid_fake = torchvision.utils.make_grid(fake, normalize=True)
-    #                 img_grid_real = torchvision.utils.make_grid(data, normalize=True)
+            ### Train Generator: min log(1 - D(G(z))) <-> max log(D(G(z))
+            # where the second option of maximizing doesn't suffer from
+            # saturating gradients
+            output = disc(fake).view(-1)
+            lossG = criterion(output, torch.ones_like(output))
+            gen.zero_grad()
+            lossG.backward()
+            opt_gen.step()
 
-    #                 writer_fake.add_image(
-    #                     "Mnist Fake Images", img_grid_fake, global_step=step
-    #                 )
-    #                 writer_real.add_image(
-    #                     "Mnist Real Images", img_grid_real, global_step=step
-    #                 )
-    #                 step += 1
+            if batch_idx == 0:
+                print(
+                    f"Epoch [{epoch}/{num_epochs}] Batch {batch_idx}/{len(loader)} \
+                        Loss D: {lossD:.4f}, loss G: {lossG:.4f}"
+                )
+
+                with torch.no_grad():
+                    fake = gen(fixed_noise).reshape(-1, 1, 28, 28)
+                    data = real.reshape(-1, 1, 28, 28)
+                    img_grid_fake = torchvision.utils.make_grid(fake, normalize=True)
+                    img_grid_real = torchvision.utils.make_grid(data, normalize=True)
+
+                    writer_fake.add_image(
+                        "Mnist Fake Images", img_grid_fake, global_step=step
+                    )
+                    writer_real.add_image(
+                        "Mnist Real Images", img_grid_real, global_step=step
+                    )
+                    step += 1
 
 
 
---
+++ ML/Pytorch/observation.md
@@ -0,0 +1,7 @@
+# Diary of my experimentations
+- Use this command to visualize your training
+```
+tensorboard --logdir=./logs
+```
+- Successfully implemented fc_gan (10/20/2023)
+
